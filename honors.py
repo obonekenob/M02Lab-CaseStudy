fb11165902e9263bf43e83540a3ecd0cf3894fdb
obonekenob <pthompson87@ivytech.edu>
2023-02-04 16:58:15 -0500

    M02 Lab - 3

--- honors.py
+++ honors.py
@@ -3,3 +3,16 @@
 # File name: honors.py  
 # Program Description:  
 
+goodToGo = True
+zstop = "ZZZ"
+
+print("This program will test if a student\'s GPA qualifies that student for ")
+print(" either the Dean\'s List or the Honor Roll.")
+print("To end the program type ZZZ when asked for the student\'s last name.")
+
+while goodToGo:
+    lastName = str(input("Enter the last name. "))
+    if lastName == zstop:
+        print("Thank you for using this program. ")
+        break
+    firstName = str(input("Enter the first name. "))
