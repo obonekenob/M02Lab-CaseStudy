413df0349050a5b43af02baf1466cc1813789f8a
obonekenob <pthompson87@ivytech.edu>
2023-02-05 20:31:47 -0500

    M02Lab-CaseStudy 3

--- honors.py
+++ honors.py
@@ -4,7 +4,10 @@
 # Program Description:  
 
 goodToGo = True
-zstop = "ZZZ"
+Bzstop = "ZZZ"
+Lzstop = "zzz"
+Dlist = 3.5
+Hlist = 3.25
 
 print("This program will test if a student\'s GPA qualifies that student for ")
 print(" either the Dean\'s List or the Honor Roll.")
@@ -12,7 +15,17 @@ print("To end the program type ZZZ when asked for the student\'s last name.")
 
 while goodToGo:
     lastName = str(input("Enter the last name. "))
-    if lastName == zstop:
+    if lastName == Bzstop or lastName == Lzstop:
         print("Thank you for using this program. ")
         break
-    firstName = str(input("Enter the first name. "))
+    else:
+        firstName = str(input("Enter the first name. "))
+        GPA = float(input("Enter the GPA: "))
+#        if GPA >= 3.5:
+        if GPA >= Dlist:
+            print("Congratulations. You are on the Dean\'s List. ")
+#        elif GPA >= 3.25:
+        elif GPA >= Hlist:
+            print("Congratulations. You made the Honor Roll.")
+        else:
+            print("I am sorry. You do not qualify for either list.")
