9522b4f48900cbc75a48bd2c32dd38d907e4d451
obonekenob <pthompson87@ivytech.edu>
2023-02-07 12:23:55 -0500

    M02Lab-CaseStudy 8

--- honors.py
+++ honors.py
@@ -40,27 +40,27 @@ hList = 3.25          #  minimum value for Honor Roll
 #
 #  Introductory comments and instructions
 #
-print("This program will test if a student\'s GPA qualifies that student for ")
+print("\n\nThis program will test if a student\'s GPA qualifies that student for ")
 print(" either the Dean\'s List or the Honor Roll.")
 print("To end the program type ZZZ when asked for the student\'s last name.")
 
 while goodToGo:        # Begin while loop
 
-    lastName = str(input("Enter the last name. "))   # Get last name or ZZZ to stop loop
+    lastName = str(input("\nEnter the last name. "))   # Get last name or ZZZ to stop loop
 
-    if lastName.upper() == zStop:                    # Check for ZZZ
-        print("Thank you for using this program. ")  # ZZZ found. Display exit message
-        goodToGo = False                             # Set loop vaiable to False and end program
+    if lastName.upper() == zStop:                          # Check for ZZZ
+        print("\nThank you for using this program.\n\n ")  # ZZZ found. Display exit message
+        goodToGo = False                                   # Set loop vaiable to False and end program
     
     else:
         firstName = str(input("Enter the student\'s first name. ")) # Get first name
         GPA = float(input("Enter the student\'s GPA: "))            # Get GPA
 
         if GPA >= dList:                                            # If GPA equal or greater 3.5
-            print("This student has made the Dean\'s List. ")       # Print deans list message and get next student
+            print("\nThis student has made the Dean\'s List. ")       # Print deans list message and get next student
 
         elif GPA >= hList:                                          # if GPA equal or greater 3.25
-            print("This student has made the Honor Roll.")          # Print honor roll message and get next student
+            print("\nThis student has made the Honor Roll.")          # Print honor roll message and get next student
 
         else:                                                       # GPA less than 3.25
-            print("This student does not qualify for either list.") # print not qualified message and get next student
+            print("\nThis student does not qualify for either list.") # print not qualified message and get next student
